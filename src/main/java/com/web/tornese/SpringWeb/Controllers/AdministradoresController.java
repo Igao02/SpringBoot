491623939678c4070300ddf89753b3249c6e97a7
Igao02 <igorabizer08@gmail.com>
2023-09-17 00:14:40 -0300

    Second Commit

--- src/main/java/com/web/tornese/SpringWeb/Controllers/AdministradoresController.java
+++ src/main/java/com/web/tornese/SpringWeb/Controllers/AdministradoresController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -26,6 +27,18 @@ public class AdministradoresController {
     @GetMapping("/administradores/novo")
     public String novo(){
         return "home/administradores/novo";
+    }
+
+    @PostMapping ("/administradores/criar")
+    public String criar(Administrador administrador){
+        repo.save(administrador);
+        return "redirect:/administradores";
+    }
+
+    @GetMapping ("/administradores/{id}/excluir")
+    public String excluir(@PathVariable int id){
+        repo.deleteById(id);
+        return "redirect:/administradores";
     }
 
 }
